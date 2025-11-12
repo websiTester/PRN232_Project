a7edd789e7a82c5bf31f57466ee249b2a3f64dea
TanPT0964 <tanpt0906@gmail.com>
2025-11-12 20:24:08 +0700

    add seller review buyer

---
+++ Frontend/Models/ReviewsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Frontend.Models
+{
+    public class ReviewsViewModel
+    {
+        public int Id { get; set; }
+        public int? ProductId { get; set; }
+        public int? ReviewerId { get; set; }
+        public int? Rating { get; set; }
+        public string? Comment { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
---
+++ Frontend/Models/SellerLeaveReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.Models
+{
+    public class SellerLeaveReviewViewModel
+    {
+        [Required]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string? Comment { get; set; }
+    }
+}
